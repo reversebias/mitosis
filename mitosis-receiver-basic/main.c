6ae103f5aa9523abf7ddee8ad2e1300d440e5871
Michael F. Lamb <mike@datagrok.org>
2017-10-29 03:30:43 -0700

    Change baud rate to 250k, which a 3.3v pro micro can support.
    Corresponding changes will be needed in the wireless firmware.

--- mitosis-receiver-basic/main.c
+++ mitosis-receiver-basic/main.c
@@ -75,7 +75,7 @@ int main(void)
           CTS_PIN_NUMBER,
           APP_UART_FLOW_CONTROL_DISABLED,
           false,
-          UART_BAUDRATE_BAUDRATE_Baud1M
+          UART_BAUDRATE_BAUDRATE_Baud250000
       };
 
     APP_UART_FIFO_INIT(&comm_params,
