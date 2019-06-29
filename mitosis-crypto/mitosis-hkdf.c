77425016249119cbbe91079c9bc4af50c44ff1e0
Anthony Rossi <anro@acm.org>
2019-06-29 02:42:43 -0700

    Add tests for HKDF functions.

--- mitosis-crypto/mitosis-hkdf.c
+++ mitosis-crypto/mitosis-hkdf.c
@@ -1,12 +1,15 @@
-#include <mitosis-hmac.h>
+
+#include <stdint.h>
+#include <stdbool.h>
 #include <string.h>
-#define SALT_VALUE "mitosis"
+#include <mitosis-hmac.h>
+#include "mitosis-hkdf.h"
 
 bool
-mitosis_hkdf_extract(const uint8_t* ikm, size_t ikm_len, uint8_t* prk) {
+mitosis_hkdf_extract(const uint8_t* ikm, size_t ikm_len, const uint8_t* salt, size_t salt_len, uint8_t* prk) {
     MITOSIS_HMAC_STATE state;
     bool result = true;
-    result = mitosis_hmac_init(&state, SALT_VALUE, sizeof(SALT_VALUE));
+    result = mitosis_hmac_init(&state, salt, salt_len);
     if(!result) {
         return result;
     }
@@ -19,34 +22,58 @@ mitosis_hkdf_extract(const uint8_t* ikm, size_t ikm_len, uint8_t* prk) {
 }
 
 bool
-mitosis_hkdf_expand(const uint8_t* prk, size_t prk_len, const char* info, size_t info_len, uint8_t* okm, size_t okm_len) {
+mitosis_hkdf_expand(const uint8_t* prk, size_t prk_len, const uint8_t* info, size_t info_len, uint8_t* okm, size_t okm_len) {
     MITOSIS_HMAC_STATE state;
     uint8_t scratch[MITOSIS_HMAC_OUTPUT_SIZE];
     bool result = true;
-    if(okm_len > sizeof(scratch)) {
+    uint8_t iterations;
+    uint16_t offset = 0;
+
+    if(okm_len > 255 * MITOSIS_HMAC_OUTPUT_SIZE) {
         return false;
+    }
+
+    if(okm_len % MITOSIS_HMAC_OUTPUT_SIZE) {
+        iterations = (uint8_t)(okm_len / MITOSIS_HMAC_OUTPUT_SIZE) + 1;
+    } else {
+        iterations = (uint8_t)(okm_len / MITOSIS_HMAC_OUTPUT_SIZE);
     }
 
     result = mitosis_hmac_init(&state, prk, prk_len);
     if(!result) {
         return result;
     }
-    result = mitosis_hmac_hash(&state, info, info_len);
-    if(!result) {
-        return result;
-    }
 
-    scratch[0] = 0x01;
-    result = mitosis_hmac_hash(&state, scratch, 1);
-    if(!result) {
-        return result;
-    }
+    for(uint8_t i = 1; i <= iterations && i > 0; ++i) {
+        if(i > 1) {
+            result = mitosis_hmac_hash(&state, scratch, sizeof(scratch));
+            if(!result) {
+                return result;
+            }
+        }
 
-    result = mitosis_hmac_complete(&state, scratch);
-    if(!result) {
-        return result;
-    }
+        result = mitosis_hmac_hash(&state, info, info_len);
+        if(!result) {
+            return result;
+        }
 
-    memcpy(okm, scratch, okm_len);
+        result = mitosis_hmac_hash(&state, &i, 1);
+        if(!result) {
+            return result;
+        }
+
+        result = mitosis_hmac_complete(&state, scratch);
+        if(!result) {
+            return result;
+        }
+
+        if(okm_len > sizeof(scratch)) {
+            memcpy(okm + offset, scratch, sizeof(scratch));
+            okm_len -= sizeof(scratch);
+            offset += sizeof(scratch);
+        } else {
+            memcpy(okm + offset, scratch, okm_len);
+        }
+    }
     return result;
 }
--- mitosis-crypto/mitosis-hkdf.h
+++ mitosis-crypto/mitosis-hkdf.h
@@ -4,11 +4,9 @@ HKDF interface for Mitosis keyboard
 Based on RFC 5869 (https://tools.ietf.org/html/rfc5869)
 */
 
+#define MITOSIS_SALT_VALUE "mitosis"
 
-bool mitosis_hkdf_extract(const uint8_t* ikm, size_t ikm_len, uint8_t* prk);
 
-/*
-Since mitosis needs less than one SHA256 hash output, this hkdf_expand() only
-produces at most 32 bytes of output.
-*/
-bool mitosis_hkdf_expand(const uint8_t* prk, size_t prk_len, const char* info, size_t info_len, uint8_t* okm, size_t okm_len);
+bool mitosis_hkdf_extract(const uint8_t* ikm, size_t ikm_len, const uint8_t* salt, size_t salt_len, uint8_t* prk);
+
+bool mitosis_hkdf_expand(const uint8_t* prk, size_t prk_len, const uint8_t* info, size_t info_len, uint8_t* okm, size_t okm_len);
