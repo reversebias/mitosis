28365778fca662aee450318c63cd9c7d2ec4e2e8
Anthony Rossi <anro@acm.org>
2019-09-20 20:37:36 -0700

    Small performance improvements.

--- mitosis-crypto/mitosis-aes-ctr.c
+++ mitosis-crypto/mitosis-aes-ctr.c
@@ -10,6 +10,25 @@ bool mitosis_aes_ctr_init(const uint8_t* key, const uint8_t* nonce, mitosis_encr
     return mitosis_aes_ecb_init(&context->ecb);
 }
 
+static inline void xor(const uint8_t* left, const uint8_t* right, size_t len, uint8_t* out)
+{
+    int idx = 0;
+    for (; len >= 4; idx += 4, len -= 4)
+    {
+        *((uint32_t*) &out[idx]) = *((uint32_t*) &left[idx]) ^ *((uint32_t*) &right[idx]);
+    }
+    if (len >= 2)
+    {
+        *((uint16_t*) &out[idx]) = *((uint16_t*) &left[idx]) ^ *((uint16_t*) &right[idx]);
+        idx += 2;
+        len -= 2;
+    }
+    if (len > 0)
+    {
+        out[idx] = left[idx] ^ right[idx];
+    }
+}
+
 bool mitosis_aes_ctr_encrypt(mitosis_encrypt_context_t* context, uint32_t datalen, const uint8_t* plaintext, uint8_t* ciphertext) {
     if(datalen > AES_BLOCK_SIZE) {
         return false;
@@ -20,9 +39,8 @@ bool mitosis_aes_ctr_encrypt(mitosis_encrypt_context_t* context, uint32_t datale
     if(!result) {
         return result;
     }
-    for(int idx = 0; idx < datalen; ++idx) {
-        ciphertext[idx] = plaintext[idx] ^ context->ctr.scratch[idx];
-    }
+    xor(plaintext, context->ctr.scratch, datalen, ciphertext);
+
     return result;
 }
 
--- mitosis-crypto/mitosis-hmac.c
+++ mitosis-crypto/mitosis-hmac.c
@@ -4,7 +4,8 @@
 #include "mitosis-hmac.h"
 #include <stdio.h>
 
-#define SHA256_OUTPUT_SIZE 32
+#define INNER_PAD 0x36
+#define OUTER_PAD 0x5c
 #define INNER_PAD_32 (uint32_t)0x36363636
 #define OUTER_PAD_32 (uint32_t)0x5c5c5c5c
 
@@ -16,32 +17,36 @@ mitosis_hmac_init(mitosis_hmac_context_t* state, const uint8_t* key, size_t len)
         return false;
     }
 
-    // create inner and outer key from key material
-    for(int i = 0; i < sizeof(state->inner_key); i += 4) {
-        *(uint32_t*)(state->inner_key + i) = INNER_PAD_32;
-    }
-
-    for(int i = 0; i < sizeof(state->outer_key); i += 4) {
-        *(uint32_t*)(state->outer_key + i) = OUTER_PAD_32;
-    }
-
     if(len > SHA256_BLOCK_SIZE) {
-        uint8_t newKey[SHA256_OUTPUT_SIZE] = { 0 };
+        uint8_t newKey[MITOSIS_HMAC_OUTPUT_SIZE] = { 0 };
         sha256_init(&(state->sha256_context));
         sha256_update(&(state->sha256_context), key, len);
         sha256_final(&(state->sha256_context), newKey);
         key = newKey;
-        len = SHA256_OUTPUT_SIZE;
+        len = MITOSIS_HMAC_OUTPUT_SIZE;
     }
 
+    // create inner and outer key from key material.
     uint32_t idx = 0;
     for(; len - idx > 4; idx += 4) {
-        *(uint32_t*)(state->inner_key + idx) ^= *(uint32_t*)(key + idx);
-        *(uint32_t*)(state->outer_key + idx) ^= *(uint32_t*)(key + idx);
+        // This does a "fast" XOR by doing 32-bits at a time.
+        *(uint32_t*)(state->inner_key + idx) = *(uint32_t*)(key + idx) ^ INNER_PAD_32;
+        *(uint32_t*)(state->outer_key + idx) = *(uint32_t*)(key + idx) ^ OUTER_PAD_32;
     }
     for(; len - idx > 0; ++idx) {
-        state->inner_key[idx] ^= key[idx];
-        state->outer_key[idx] ^= key[idx];
+        // Fallback to "slow" XOR for the rest of the key material.
+        state->inner_key[idx] = key[idx] ^ INNER_PAD;
+        state->outer_key[idx] = key[idx] ^ OUTER_PAD;
+    }
+    for(; (idx & 0x3) && idx < sizeof(state->inner_key); ++idx) {
+        // "Slow" copy to the next 4-byte boundary.
+        state->inner_key[idx] = INNER_PAD;
+        state->outer_key[idx] = OUTER_PAD;
+    }
+    for(; idx < sizeof(state->inner_key); idx += 4) {
+        // "Fast" copy the rest of the key material.
+        *(uint32_t*)(state->inner_key + idx) = INNER_PAD_32;
+        *(uint32_t*)(state->outer_key + idx) = OUTER_PAD_32;
     }
 
     sha256_init(&(state->sha256_context));
@@ -69,7 +74,7 @@ mitosis_hmac_complete(mitosis_hmac_context_t* state, uint8_t* hash) {
     if(state == 0 || hash == 0) {
         return false;
     }
-    uint8_t first_hash[SHA256_OUTPUT_SIZE];
+    uint8_t first_hash[MITOSIS_HMAC_OUTPUT_SIZE];
     state->need_reset = 1;
     sha256_final(&(state->sha256_context), first_hash);
     // Re-use the hash object to compute the 2nd hash pass.
